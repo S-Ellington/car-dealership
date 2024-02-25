984e5b086b6b47acb81cb76b59c574739081c98f
Shenedia Ellington <artistic.nita@gmail.com>
2024-02-24 23:44:33 -0500

    structure of lab

---
+++ main.py
@@ -0,0 +1,43 @@
+bikes = ['Chopper', 'Harley-Davidson', 'Bobber']
+trucks = ['Ford Maverick', 'Hyundai Santa Cruz', 'Chevrolet Colorado']
+
+
+class Vehicle:
+    vehicles_to_compare = []
+    def __init__(self, make, miles, price, engine_on=False):
+        self.make = make
+        self.miles = miles
+        self.price = price
+        self.engine_on = False
+        pass
+    def start_engine(self):
+        print("Starting engine...")
+        #  Sets engine_on to True
+        pass
+    def make_noise(self):
+        print("Beep beep!")
+        pass
+
+class Truck(Vehicle):
+    def __init__(self, make, miles, price, cargo=False):
+        super().__init__(make, miles, price)
+        self.cargo = False
+        pass
+    def load_cargo(self):
+        print("Loading the truck bed...")
+        #  Sets cargo to True
+class Motorcycle(Vehicle):
+    def __init__(self, make, miles, price, top_speed):
+        super().__init__(make, miles, price)
+        self.top_speed = top_speed
+    def make_noise(self):
+        print("Vroom vroom!")
+        pass
+
+
+# motor = Motorcycle()
+# motor.make_noise()
+
+
+while True:
+    pass
